eff301b29077305754d490643e06688afccd8926
DanielWil252 <dan.rei.wilcox@gmail.com>
2024-02-15 22:25:15 -0500

    hw 2 progress

--- all_questions.py
+++ all_questions.py
@@ -23,40 +23,40 @@ def question1():
     level2_left = {}
     level2_right = {}
 
-    level1["smoking"] = 0.
-    level1["smoking_info_gain"] = 0.
+    level1["smoking"] = 1.
+    level1["smoking_info_gain"] = 0.9278071905112637
 
-    level1["cough"] = 0.
-    level1["cough_info_gain"] = 0.
+    level1["cough"] = -1
+    level1["cough_info_gain"] = -1
 
-    level1["radon"] = 0.
-    level1["radon_info_gain"] = 0.
+    level1["radon"] = -1
+    level1["radon_info_gain"] = -1
 
-    level1["weight_loss"] = 0.0
-    level1["weight_loss_info_gain"] = 0.
+    level1["weight_loss"] = -1
+    level1["weight_loss_info_gain"] = -1
 
-    level2_left["smoking"] = 0.
-    level2_left["smoking_info_gain"] = 0.
-    level2_right["smoking"] = 0.
-    level2_right["smoking_info_gain"] = 0.
+    level2_left["smoking"] = -1
+    level2_left["smoking_info_gain"] = -1
+    level2_right["smoking"] = -1
+    level2_right["smoking_info_gain"] = -1
 
-    level2_left["radon"] = 0.
-    level2_left["radon_info_gain"] = 0.
+    level2_left["radon"] = -1
+    level2_left["radon_info_gain"] = -1
 
-    level2_left["cough"] = 0.
-    level2_left["cough_info_gain"] = 0.
+    level2_left["cough"] = 1
+    level2_left["cough_info_gain"] = 0.7219280948873623
 
-    level2_left["weight_loss"] = 0.
-    level2_left["weight_loss_info_gain"] = 0.
+    level2_left["weight_loss"] = -1
+    level2_left["weight_loss_info_gain"] = -1
 
-    level2_right["radon"] = 0.
-    level2_right["radon_info_gain"] = 0.
+    level2_right["radon"] = 1
+    level2_right["radon_info_gain"] = 0.7219280948873623
 
-    level2_right["cough"] = 0.
-    level2_right["cough_info_gain"] = 0.
+    level2_right["cough"] = -1
+    level2_right["cough_info_gain"] = -1
 
-    level2_right["weight_loss"] = 0.
-    level2_right["weight_loss_info_gain"] = 0.
+    level2_right["weight_loss"] = -1
+    level2_right["weight_loss_info_gain"] = -1
 
     answer["level1"] = level1
     answer["level2_left"] = level2_left
@@ -64,7 +64,13 @@ def question1():
 
     # Fill up `construct_tree``
     # tree, training_error = construct_tree()
-    tree = u.BinaryTree("root")  # MUST STILL CREATE THE TREE *****
+    tree = u.BinaryTree("smoke")  # MUST STILL CREATE THE TREE *****
+    A = tree.insert_left("cough")
+    A.insert_left("y")
+    A.insert_right("n")
+    B = tree.insert_right("radon")
+    B.insert_left("y")
+    B.insert_right("y")
     answer["tree"] = tree  # use the Tree structure
     # answer["training_error"] = training_error
     answer["training_error"] = 0.0  
@@ -79,18 +85,31 @@ def question2():
     answer = {}
 
     # Answers are floats
-    answer["(a) entropy_entire_data"] = 0.
+    answer["(a) entropy_entire_data"] = 1.4253642047367425
     # Infogain
-    answer["(b) x <= 0.2"] = 0.
-    answer["(b) x <= 0.7"] = 0.
-    answer["(b) y <= 0.6"] = 0.
+    answer["(b) x <= 0.2"] = 0.17739286055515846
+    answer["(b) x <= 0.7"] = 0.3557029418697566
+    answer["(b) y <= 0.6"] = 1.1206115813673811
 
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
-    answer["(c) attribute"] = ""  
+    answer["(c) attribute"] = "y=0.6"  
 
     # Use the Binary Tree structure to construct the tree
     # Answer is an instance of BinaryTree
-    tree = u.BinaryTree("Root")
+    tree = u.BinaryTree("y<=0.6")
+
+    A = tree.insert_left("x<=0.7")
+    A.insert_left("B")
+    C = A.insert_right("y<=0.3")
+    C.insert_left("A")
+    C.insert_right("C")#pure nodes
+
+    B = tree.insert_right("x<=0.2")
+    B.insert_right("A")
+    D = B.insert_left("y<=0.8")
+    D.insert_left("C")
+    D.insert_right("B")#pure nodes
+
     answer["(d) full decision tree"] = tree
 
     return answer
@@ -103,18 +122,18 @@ def question3():
     answer = {}
 
     # float
-    answer["(a) Gini, overall"] = 0.
+    answer["(a) Gini, overall"] = 0.5
 
     # float
-    answer["(b) Gini, ID"] = 0.0
-    answer["(c) Gini, Gender"] = 0.
-    answer["(d) Gini, Car type"] = 0.
-    answer["(e) Gini, Shirt type"] = 0.
+    answer["(b) Gini, ID"] = 0.
+    answer["(c) Gini, Gender"] = 0.48
+    answer["(d) Gini, Car type"] = 0.1625
+    answer["(e) Gini, Shirt type"] = 0.4914
 
-    answer["(f) attr for splitting"] = ""
+    answer["(f) attr for splitting"] = "Car"
 
     # Explanatory text string
-    answer["(f) explain choice"] = ""
+    answer["(f) explain choice"] = "It has the lowest gini value."
 
     return answer
 
@@ -135,46 +154,46 @@ def question4():
     #  'quantitative', 'interval', 'ratio'
     # If you have a choice between 'binary' and 'discrete', choose 'binary'
 
-    answer["a"] = []
+    answer["a"] = ['binary','qualitative','ordinal']
 
     # Explain if there is more than one interpretation. Repeat for the other questions. At least five words that form a sentence.
-    answer["a: explain"] = ""
+    answer["a: explain"] = "AM and PM are binary representations of time. They dont distincly represent numbers, so they are qualitative. Since AM naturally comes first before PM, it is ordinal."
 
-    answer["b"] = []
-    answer["b: explain"] = ""
+    answer["b"] = ['continuous','quantitative','ratio']
+    answer["b: explain"] = "Light meters measure light in continous numerical values, and there is a clear zero."
 
-    answer["c"] = []
-    answer["c: explain"] = ""
+    answer["c"] = ['discrete','qualitative','ordinal']
+    answer["c: explain"] = "People measuring light based on their eyesight would wont have specific values in mind, but generalizations. The generalizations are ordinal since they would still be ranked by the brightness of light."
 
-    answer["d"] = []
-    answer["d: explain"] = ""
+    answer["d"] = ['continuous','quantitative','ratio']
+    answer["d: explain"] = "Degrees can be continuous, and have a specific zero point."
 
-    answer["e"] = []
-    answer["e: explain"] = ""
+    answer["e"] = ['discrete','qualitative','ordinal']
+    answer["e: explain"] = "The medals themselves do not represent any numerical value, so they are qualitative. They are ordinal because there is a distinct ranking between their value."
 
-    answer["f"] = []
-    answer["f: explain"] = ""
+    answer["f"] = ['countinuous','quantitative','interval']
+    answer["f: explain"] = "The height above sea level is measured in a continuous, quantitative manner. However, ratio vs interval can be debated because zero sea level can be different around the world."
 
-    answer["g"] = []
-    answer["g: explain"] = ""
+    answer["g"] = ['discrete','quantitative','ratio']
+    answer["g: explain"] = "the amount of patients in a hospital is discrete and measured numerically. There can also be zero patients, so it can be ratioed."
 
-    answer["h"] = []
-    answer["h: explain"] = ""
+    answer["h"] = ['discrete','qualitative','nominal']
+    answer["h: explain"] = "isbn numbers dont necessarily represent anything other than the book they are assigned to. there also isnt a clear ranking for them (Im not sure if isbn numbers are in order of release)"
 
-    answer["i"] = []
-    answer["i: explain"] = ""
+    answer["i"] = ['discrete','qualitative','ordinal']
+    answer["i: explain"] = "the words used to describe the light cannot be continuous, and dont hold a specific value. They can, however, can be ranked in order."
 
-    answer["j"] = []
-    answer["j: explain"] = ""
+    answer["j"] = ['discrete','qualitative','ordinal']
+    answer["j: explain"] = "Military ranks are clear in level of power, but dont represent numbers and are not continuous."
 
-    answer["k"] = []
-    answer["k: explain"] = ""
+    answer["k"] = ['continuous','quantitative','ratio']
+    answer["k: explain"] = "the distance from a certain point is continuous and holds numerical value. Whether or not it is a ratio or an interval depends on whether that distance is considered a zero point or not (I think it is in this case)"
 
-    answer["l"] = []
-    answer["l: explain"] = ""
+    answer["l"] = ['discrete','quantitative','ratio']
+    answer["l: explain"] = "density is a measurement that is continuous and quantitative. There is also a set zero point."
 
-    answer["m"] = []
-    answer["m: explain"] = ""
+    answer["m"] = ['discrete','qualitative','nominal']
+    answer["m: explain"] = "A count number would have to be discrete and does not really represent anything numerically. Since Id like to imagine there isnt an order of coats, it would be nominal."
 
     return answer
 
@@ -188,18 +207,18 @@ def question5():
     # Read appropriate section of book chapter 3
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["a"] = ""
-    explain["a explain"] = ""
+    explain["a"] = "Model 2"
+    explain["a explain"] = "While model 1 is more accurate in on Dataset A, that dataset was used to train the data itself. Since dataset B is a seperate testing set, Model 2's higher score in classification accuracy than A in this dataset shows that it would be more accurate for new data than model 1."
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["b"] = ""
-    explain["b explain"] = ""
+    explain["b"] = "Model 1"
+    explain["b explain"] = "Given just the accuracies, I would choose model 1 over model 2."
 
     explain["c similarity"] = ""
-    explain["c similarity explain"] = ""
+    explain["c similarity explain"] = "Both MDL and the pessimistic error estimate aim to prevent overfitting by penalizing the model if it is overcomplex."
 
     explain["c difference"] = ""
-    explain["c difference explain"] = ""
+    explain["c difference explain"] = "MDL and pessimistic error estimate operate differently to achieve a similar goal. MDL achieves this by selecting a model that compresses the data to the smallest size, while pessimistic error estimate directly adds penalties to the error rate based on the complexity of the model."
 
     return explain
 
@@ -238,19 +257,19 @@ def question7():
     answer = {}
 
     # float
-    answer["a, info gain, ID"] = 0.
-    answer["b, info gain, Handedness"] = 0.
+    answer["a, info gain, ID"] = 1 #same as root entropy, since entropy for ID is 0
+    answer["b, info gain, Handedness"] = 0.9531004406410719
 
     # string: "ID" or "Handedness"
-    answer["c, which attrib"] = ""
+    answer["c, which attrib"] = "ID"
 
     # answer is a float
-    answer["d, gain ratio, ID"] = 0.
-    answer["e, gain ratio, Handedness"] = 0.
+    answer["d, gain ratio, ID"] = 0.23137821315975915
+    answer["e, gain ratio, Handedness"] = 0.4321928094887363
 
     # string: one of 'ID' or 'Handedness' based on gain ratio
     # choose the attribute with the largest gain ratio
-    answer["f, which attrib"] = ""
+    answer["f, which attrib"] = "Handedness"
 
     return answer
 
