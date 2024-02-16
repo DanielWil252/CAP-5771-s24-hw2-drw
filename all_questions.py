a5cf88b26a8f3242d8140a5acdfa66f63e204bf6
DanielWil252 <dan.rei.wilcox@gmail.com>
2024-02-15 23:48:34 -0500

    q2 potential fix 2

--- all_questions.py
+++ all_questions.py
@@ -88,8 +88,8 @@ def question2():
     answer["(a) entropy_entire_data"] = 1.4253642047367425
     # Infogain
     answer["(b) x < 0.2"] = 0.17739286055515846
-    answer["(b) x <= 0.7"] = 0.3557029418697566
-    answer["(b) y <= 0.6"] = 1.1206115813673811
+    answer["(b) x < 0.7"] = 0.3557029418697566
+    answer["(b) y < 0.6"] = 1.1206115813673811
 
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
     answer["(c) attribute"] = "y=0.6"  
